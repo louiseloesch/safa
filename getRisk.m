4f9eeeda74d55ff77f89ba783d571bff1c1a7a44
gpatonay <pg@localhost.localdomain>
2017-11-15 15:37:36 +0100

    handle flame sensor in getRisk()

--- getRisk.m
+++ getRisk.m
@@ -1,7 +1,13 @@
 function severity=getRisk( meas )
 
-severity=0;
-meas(end)
-severity=double( ( check_temperature(meas,1) > 30 )&&( humidity(meas,4) < 30 ) );
+severity=1;
+
+severity=severity&&( check_temperature(meas,1) > 30 ); % risk
+severity=severity&&( humidity(meas,4) < 30 ); % risk
+if meas(end).flame < 2 % fire
+    severity=2;
+end
+
+severity=double( severity );
 
 end
