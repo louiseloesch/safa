e58508f54a8f859412120f36bf580514fe38eac6
louiseloesch <louise.loesch@telecom-paristech.fr>
2017-11-15 10:37:58 +0100

    calculate the average of humidity

---
+++ humidity.m
@@ -0,0 +1,25 @@
+function humidity_average=check_humidity(average,epsilon)
+value=meas(end).humidity(end);
+humidity_average=average;
+if (average-epsilon<=value&&value<=average+epsilon)
+    humidity_average=(average+value)/2;
+    if value < average
+    disp('the weather is getting dryer');
+    else
+    disp('the weather is getting wetter');
+    end
+else
+    if (meas(end-1).humidity-epsilon<=value&&value<=meas(end-1).humidity+epsilon)
+        humidity_average=(average+value)/2;
+        if value < average
+            disp('the weather is getting dryer');
+        else
+            disp('the weather is getting wetter');
+        end
+    else
+        meas(end).humidity=[];
+    end
+end
+
+end
+
