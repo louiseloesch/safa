72f08bce70cc9272ca25d2d696350b295465e618
gpatonay <pg@localhost.localdomain>
2017-11-15 16:10:19 +0100

    handle connection error to arduino

--- initArduino.m
+++ initArduino.m
@@ -1,8 +1,13 @@
 function a=initArduino()
+
+try
     instrreset
 
     a = serial('COM5');
     set(a,'BaudRate',115200);
     fopen(a);
+catch
+    a=[];
+end
 
 end
--- readMeasurements.m
+++ readMeasurements.m
@@ -1,8 +1,12 @@
 function meas=readMeasurements( arduino )
 % returns the measured values from Aurduino
 
-out = fscanf(arduino);
-
-meas = sscanf(out,'F%dG%dH%fT%f') % fire - gas - humidity - temperature
+meas=zeros(4,1);
+if ~isempty( arduino )
+    out = fscanf(arduino);
+    meas = sscanf(out,'F%dG%dH%fT%f') % fire - gas - humidity - temperature
+else
+    disp( 'Could not access arduino, measuremnts sets to 0!' );
+end
 
 end
